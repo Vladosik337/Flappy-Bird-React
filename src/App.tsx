3b6561e1dcdccf97f77b92976dac63287e2987f9
Vladosik337 <vlad.ostapenko.337@gmail.com>
2024-10-08 01:08:20 +0300

    [FEAT] create architecture components

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import './styles/style.scss';
 function App() {
     return (
         <div className="min-h-full flex flex-col items-center justify-center overflow-clip h-screen">
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+
         </div>
     );
 }
---
+++ src/components/gameContainer/ground/Ground.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ground = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Ground;
